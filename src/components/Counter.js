9324005889822cd2aa1c0544557dfa210891a0b6
Byungchan Lim <bl458@cornell.edu>
2020-08-27 09:38:33 +0900

    fix: Fix Counter styling

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,10 +1,10 @@
 import React, {Component} from 'react';
-import {Button} from 'react-native';
+import {Button, View, Text} from 'react-native';
 
 export default class Counter extends Component {
   render() {
     return (
-      <View>
+      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
         <Button onPress={this.props.increment} title="Increase Count" />
 
         <Text>{this.props.count}</Text>
