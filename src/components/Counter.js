7326099532340f3bf2dde27dd57b729555d3dcfc
Byungchan Lim <bl458@cornell.edu>
2020-08-27 09:37:34 +0900

    feat: Implement Counter component

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,3 +1,16 @@
 import React, {Component} from 'react';
+import {Button} from 'react-native';
 
-export default class Counter extends Component {}
+export default class Counter extends Component {
+  render() {
+    return (
+      <View>
+        <Button onPress={this.props.increment} title="Increase Count" />
+
+        <Text>{this.props.count}</Text>
+
+        <Button onPress={this.props.decrement} title="Decrease Count" />
+      </View>
+    );
+  }
+}
